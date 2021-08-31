2d9ed9b458745cf49c96871ee5bfe944538da7ea
Manuel <manuel@somdata.es>
2021-08-31 09:46:12 +0200

    Product and rating components created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Product from './components/Product.js';
 import data from './data.js';
 
 function App() {
-  return (
-    <div className="grid-container">
+    return (
+        <div className="grid-container">
             <header className="row">
                 <div>
                     <a className="brand" href="/">amazona</a>
@@ -15,35 +16,18 @@ function App() {
             </header>
             <main>
                 <div className="row center">
-                  {
-                    data.products.map(product => (
-                      <div key={product._id} className="card">
-                      <a href={`/product/${product._id}`}>
-                          <img className="medium" src={product.image} alt={product.name} />
-                      </a>
-                      <div className="card-body">
-                          <a href={`/product/${product._id}`}>
-                              <h2>{product.name}</h2>
-                          </a>
-                          <div className="rating">
-                              <span><i className="fa fa-star"></i></span>
-                              <span><i className="fa fa-star"></i></span>
-                              <span><i className="fa fa-star"></i></span>
-                              <span><i className="fa fa-star"></i></span>
-                              <span><i className="fa fa-star"></i></span>
-                          </div>
-                          <div className="price">${product.price}</div>
-                      </div>
-                  </div>))
-                  }
-                    
+                    {
+                        data.products.map((product) => (
+                            <Product key={product._id} product={product} />
+                        ))
+                    }
                 </div>
             </main>
             <footer className="row center">
                 All rights reserved
             </footer>
         </div>
-  );
+    );
 }
 
 export default App;
